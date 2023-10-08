b8b23053e85b65620f6857cdfb2266bcfda84540
ccozad <ccozad@gmail.com>
2023-10-08 00:25:37 -0700

    Adjust default map size

--- src/MapTest.js
+++ src/MapTest.js
@@ -5,7 +5,7 @@ export default function MapTest() {
     return (
         <div>
             <h1>This is where we will experiment with map mechanics </h1>
-            <Map rows={10} columns={10} />
+            <Map rows={15} columns={30} />
         </div>
     );
 }
