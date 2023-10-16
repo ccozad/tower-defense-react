035cd5e94bfe625a78a48ff965ac19803354cfc3
ccozad <ccozad@gmail.com>
2023-10-16 00:54:47 -0700

    Waypoint list movement is working

--- src/OrdinaryEnemy.js
+++ src/OrdinaryEnemy.js
@@ -9,24 +9,103 @@ function OrdinaryEnemy({
     texture, 
     textureDebug = false, 
     showBorder = false,
-    x = 0,
-    y = 0,
-    rotation = 0
+    wayPoints = [],
+    targetWayPoint = 1
 }) {
-    let className = showBorder? "enemy-border" : "enemy"
-    let css = {};
-    if (Object.prototype.hasOwnProperty.call(textureMap, texture)) {
-        css = {
-            "backgroundPosition": `${textureMap[texture].x}px ${textureMap[texture].y}px`,
-            "left": `${x}px`,
-            "top": `${y}px`,
-            "transform": `rotate(${-rotation}deg)`
-        };
+    const x = React.useRef(0);
+    const y = React.useRef(0);
+    const rotation = React.useRef(0);
+    const distanceTraveled = React.useRef(0);
+    const speed = React.useRef(1);
+    const distance = React.useRef(0);
+    const direction = React.useRef({x: 0, y: 0});
+    const enemyRef = React.useRef(null);
+    const start = React.useRef(0);
+    const end = React.useRef(1);
+    const css = {
+        "backgroundPosition": `${textureMap[texture].x}px ${textureMap[texture].y}px`
+    };
+    const className =showBorder? "enemy-border" : "enemy";
+
+    function animLoop(renderLocal, element) {
+        var running, lastFrame = +new Date;
+        function loop( now ) {
+            if ( running !== false ) {
+                requestAnimationFrame( loop, element );
+                var deltaT = now - lastFrame;
+                // do not render frame when deltaT is too high
+                if ( deltaT < 160 ) {
+                    running = renderLocal( deltaT );
+                }
+                lastFrame = now;
+            }
+        }
+        loop( lastFrame );
     }
+
+    function render() {
+        const dx = x.current - wayPoints[start.current].x;
+        const dy = y.current - wayPoints[start.current].y;
+        distanceTraveled.current = Math.sqrt(dx*dx + dy*dy);
+
+        if ( distanceTraveled.current >= distance.current ) {
+            return false;
+        } else {
+            //x.current += speed.current * deltaT;
+            x.current += speed.current * direction.current.x;
+            y.current += speed.current * direction.current.y;
+            /*if (x.current > wayPoints[end.current].x || y.current > wayPoints[end.current].y) {
+                x.current = wayPoints[end.current].x;
+                y.current = wayPoints[end.current].y;
+            }*/
+            enemyRef.current.style.left = `${x.current}px`;
+            enemyRef.current.style.top = `${y.current}px`;
+        }
+    }
+
+    /*React.useEffect(() => {
+        const dx = endPosition.x - startPosition.x;
+        const dy = endPosition.y - startPosition.y;
+        distance.current = Math.sqrt(dx*dx + dy*dy);
+        direction.current = {x: dx / distance.current, y: dy / distance.current};
+        rotation.current = Math.atan2(dy, dx) * 180 / Math.PI;
+        enemyRef.current.style.transform = `rotate(${rotation.current}deg)`;
+        animLoop(render, enemyRef.current);
+    }, []);*/
+
+    React.useEffect(() => {
+        console.log('Target way point changed');
+        console.log(`targetWayPoint: ${targetWayPoint}`);
+        if ( wayPoints.length > 1) {
+            start.current = (targetWayPoint - 1) % wayPoints.length;
+            end.current = targetWayPoint % wayPoints.length;
+            x.current = wayPoints[start.current].x;
+            y.current = wayPoints[start.current].y;
+            enemyRef.current.style.left = `${wayPoints[start.current].x}px`;
+            enemyRef.current.style.top = `${wayPoints[start.current].y}px`;
+            const dx = wayPoints[end.current].x - wayPoints[start.current].x;
+            const dy = wayPoints[end.current].y - wayPoints[start.current].y;
+            distance.current = Math.sqrt(dx*dx + dy*dy);
+            direction.current = {x: dx / distance.current, y: dy / distance.current};
+            rotation.current = Math.atan2(dy, dx) * 180 / Math.PI;
+            enemyRef.current.style.transform = `rotate(${rotation.current}deg)`;
+            animLoop(render, enemyRef.current);
+        }
+    }, [targetWayPoint]);
+
+    /*function refresh() {
+        setClassName(showBorder? "enemy-border" : "enemy");
+        setCss({
+            "backgroundPosition": `${textureMap[texture].x}px ${textureMap[texture].y}px`,
+            "left": `${x.current}px`,
+            "top": `${y.current}px`,
+            "transform": `rotate(${rotation.current}deg)`
+        });
+    }*/
     
     return (
         <>
-            <div className={className} style={css}>{textureDebug ? texture: ''}</div>
+            <div ref={enemyRef} className={className} style={css}>{textureDebug ? texture: ''}</div>
         </>
     );
 }
@@ -35,9 +114,8 @@ OrdinaryEnemy.propTypes = {
     texture: PropTypes.number.isRequired,
     textureDebug: PropTypes.bool,
     showBorder: PropTypes.bool,
-    x: PropTypes.number,
-    y: PropTypes.number,
-    rotation: PropTypes.number
+    wayPoints: PropTypes.array,
+    targetWayPoint: PropTypes.number
 };
 
 export default OrdinaryEnemy;
--- src/WayPointTest.js
+++ src/WayPointTest.js
@@ -12,27 +12,17 @@ import { useState } from 'react';
     currentPosition = {x: x, y: y};
     //console.log(currentPosition);
 }*/
-
-
   
 export default function WayPointTest() {
     const wayPoints = [
         {x: 0, y: 0},
-        {x: 30, y: 30},
-        {x: 500, y: 500}
+        {x: 100, y: 100},
+        {x: 300, y: 0},
+        {x: 300, y: 300},
+        {x: 50, y: 200},       
     ];
 
-    const [angle, setAngle] = useState(0);
-    const [started, setStarted] = useState(false);
-    const [timer, setTimer] = useState(0);
-    //const [startWayPoint, setStartWayPoint] = useState(0);
-    //const [destinationWayPoint, setDestinationWayPoint] = useState(1);
-    // Unit of speed is px/ms
-    //const [speed, setSpeed] = useState(1);
-    const [currentPosition, setCurrentPosition] = useState(wayPoints[0]);
-    //const [startTime, setStartTime] = useState(0);
-    //const [endTime, setEndTime] = useState(0);
-    //const [elapsedTime, setElapsedTime] = useState(0);
+    const [targetWayPoint, setTargetWayPoint] = useState(1);
 
     // start an waypoint n
     // Move towards way point n + 1
@@ -56,77 +46,26 @@ export default function WayPointTest() {
     //
     // React may batch multiple setState() calls into a single update for performance.
     // Because this.props and this.state may be updated asynchronously, 
-    // you should not rely on their values for calculating the next state.
+    // you should not rely on their values for calculating the next state
 
-    function moveTowardWayPoint() {
-        //setEndTime(Date.now());
-        //setElapsedTime(endTime - startTime); // ms
-        let dx = wayPoints[1].x - currentPosition.x;
-        let dy = wayPoints[1].y - currentPosition.y;
-        //let distance = Math.sqrt(dx*dx + dy*dy); // px
-        //console.log("Distance: " + distance);
-
-        //if (distance < 5) {
-        //    console.log("Reached way point");
-        //    setCurrentPosition(wayPoints[destinationWayPoint]);
-        //    setStartWayPoint((startWayPoint) => (startWayPoint + 1) % wayPoints.length);
-        //    setDestinationWayPoint((destinationWayPoint) => (destinationWayPoint + 1) % wayPoints.length);
-        //} else {
-            let newAngle  = Math.atan2(-dy, dx) * 180 / Math.PI; // deg
-
-            setAngle(newAngle);
-            setCurrentPosition((currentPosition) => {
-                return {
-                    x: currentPosition.x + 1, 
-                    y: currentPosition.y + 1
-                };
-            });
-        //}
-
-        //setStartTime(endTime);
+    function nextWayPoint() {
+        setTargetWayPoint((targetWayPoint) => (targetWayPoint + 1));
     }
     
-    function start() {
-        if(!started){
-            //setStartTime(Date.now());
-            setTimer(setInterval(moveTowardWayPoint, 1000/24));
-            setStarted(true);
-        }
-    }
-    
-    function stop() {
-        clearInterval(timer);
-        setStarted(false);
-    }
     return (
         <>
             <div>
-                <p>Waypoint Controls</p>
-                <button onClick={() => start()}>start</button>
-                <button onClick={() => stop()}>Stop</button>
-                <button onClick={() => {
-                    stop();
-                    setAngle(0); 
-                    setCurrentPosition(wayPoints[0]);
-                    //setStartWayPoint(0);
-                    //setDestinationWayPoint(1);
-                    //setElapsedTime(0);
-                    //setStartTime(0);
-                    //setEndTime(0);
-                    //setSpeed(1);
-                }}>Reset</button>
-            </div>
-            <div>
-                <p>Angle: {angle} Position: ( {currentPosition.x.toFixed(0)}, {currentPosition.y.toFixed(0)} )</p>
+                <p>Way Point Controls</p>
+                <button onClick={() => { nextWayPoint()}}>Next Way Point</button>
             </div>
             <div>
                 <OrdinaryEnemy 
                     texture={1} 
                     textureDebug={false} 
                     showBorder={true} 
-                    x={currentPosition.x}
-                    y={currentPosition.y}
-                    rotation={angle}/>
+                    wayPoints={wayPoints}
+                    targetWayPoint={targetWayPoint}
+                />
             </div>
             
         </>
